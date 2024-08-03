892dcd4f5426e7eff256f02f5889001fc511d1bb
varun17090 <vpati5446@gmail.com>
2024-08-03 13:55:18 +0530

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,14 @@
+print("1.Addition\n2.Substration\n3.Multiply")
+choice=int(input("Enter Your choice"))
+a=int(input("Enter Your First Number :-"))
+b=int(input("Enter Your Second Number :-"))
+if (choice==1):
+  print(a+b)
+elif(choice==2):
+  print(a-b)
+elif(choice==3):
+  print(a*b)
+else:
+  print("You Have Enter Wrong Choice")
+  
+  
